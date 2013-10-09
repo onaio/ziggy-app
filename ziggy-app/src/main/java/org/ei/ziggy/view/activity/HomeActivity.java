71b029dc83f0cf5e75e89d7a5ad9f4651e6c09da
Peter Pistorius <peter.pistorius@gmail.com>
2013-10-09 14:25:33 +0100

    use clts instead of index.

--- ziggy-app/src/main/java/org/ei/ziggy/view/activity/HomeActivity.java
+++ ziggy-app/src/main/java/org/ei/ziggy/view/activity/HomeActivity.java
@@ -3,6 +3,6 @@ package org.ei.ziggy.view.activity;
 public class HomeActivity extends WebFormActivity {
     @Override
     protected void onInitialization() {
-        webView.loadUrl("file:///android_asset/www/home.html");
+        webView.loadUrl("file:///android_asset/clts/index.html");
     }
 }
